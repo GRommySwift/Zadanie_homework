9ad4f98cb5bdeb194a2cba199c4de463ced2a1db
Roman <romaholovai@gmail.com>
2024-10-15 20:40:48 +0200

    initial commit

---
+++ FakeStore/FakeStore/FakeStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  FakeStoreApp.swift
+//  FakeStore
+//
+//  Created by Roman on 15/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct FakeStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
