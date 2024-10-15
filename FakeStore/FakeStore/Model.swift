78fa1974cafba416f4d0ddc3585eaea747641a09
Roman <romaholovai@gmail.com>
2024-10-15 21:07:31 +0200

    has added Alamofire&PromiseKit packages, created model, added NetworkManager(but non-configured)

---
+++ FakeStore/FakeStore/Model.swift
@@ -0,0 +1,34 @@
+//
+//  Model.swift
+//  FakeStore
+//
+//  Created by Roman on 15/10/2024.
+//
+
+import Foundation
+
+// MARK: - WelcomeElement
+struct WelcomeElement: Codable {
+    let id: Int
+    let title: String
+    let price: Double
+    let description: String
+    let category: Category
+    let image: String
+    let rating: Rating
+}
+
+enum Category: String, Codable {
+    case electronics = "electronics"
+    case jewelery = "jewelery"
+    case menSClothing = "men's clothing"
+    case womenSClothing = "women's clothing"
+}
+
+// MARK: - Rating
+struct Rating: Codable {
+    let rate: Double
+    let count: Int
+}
+
+//typealias Welcome = [WelcomeElement]
